28f0851d9964184e8e3dbbf09360e21c22e12875
Berat <95562697+BeratSJ@users.noreply.github.com>
2026-07-01 14:27:17 +0300

    socket - client.c and server.c edited

--- socket/client.c
+++ socket/client.c
@@ -1,34 +1,40 @@
 #include <stdio.h>
 #include <string.h>
-#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 #include <arpa/inet.h>
+#include <unistd.h>
 
-int main() {
-
-    int sock;
-
+int main(){
+    int sockfd = socket(AF_INET, SOCK_STREAM,0); // AF_INET=IPv4 SOCK_STREAM=TCP protocol=0
+    if (sockfd == -1){  // Eğer hata verirse -1 döner ve bunu göstermek için bunu yazdık.
+        perror("socket error");
+        return 1;
+    }
     struct sockaddr_in server_addr;
-
-    char *msg = "Hello Server";
-
-    char buffer[1024] = {0};
-
-    sock = socket(AF_INET, SOCK_STREAM, 0);
-
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(8080);
-
+    server_addr.sin_family = AF_INET; // IPv4
+    server_addr.sin_port = htons(8080); //port 8080
     inet_pton(AF_INET, "127.0.0.1", &server_addr.sin_addr);
 
-    connect(sock, (struct sockaddr*)&server_addr, sizeof(server_addr));
+    int result = connect(sockfd, (struct sockaddr*)&server_addr,sizeof(server_addr));
+    if (result == -1){
+        perror("result error");
+        return 1;
+    }
 
-    send(sock, msg, strlen(msg), 0);
+    char buffer[1024];
 
-    recv(sock, buffer, sizeof(buffer), 0);
+        char message[1024];
+    printf("Write Message:");
+    fgets(message, sizeof(message),stdin);
+    send(sockfd,message, strlen(message), 0);
 
-    printf("Server: %s\n", buffer);
+    int bytes_received = recv(sockfd, buffer, sizeof(buffer), 0);  // mesaj alımı
+    buffer[bytes_received] = '\0';  //sondaki oluşan gereksiz kısmı siliyoruz
 
-    close(sock);
+    printf("Sunucudan gelen: %s\n", buffer);
+
+    close(sockfd);
 
     return 0;
 }
--- socket/server.c
+++ socket/server.c
@@ -1,38 +1,55 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 #include <arpa/inet.h>
+#include <unistd.h>
 
-int main() {
-    int server_fd;
-    int client_fd;
+int main(){
+    int sockfd = socket(AF_INET,SOCK_STREAM,0); // AF_INET=IPv4 SOCK_STREAM=TCP protocol=0
+    if (sockfd == -1){  // Eğer hata verirse -1 döner ve bunu göstermek için bunu yazdık.
+        perror("socket error");
+        return 1;
+    }
+    struct sockaddr_in server_addr;
+    server_addr.sin_family = AF_INET; // IPv4
+    server_addr.sin_port = htons(8080); //port 8080
+    server_addr.sin_addr.s_addr = INADDR_ANY; //bütün arayüzlerden dinle
 
-    struct sockaddr_in addr;
+    int result = bind(sockfd,(struct sockaddr*)&server_addr, sizeof(server_addr)); //port'u atar.
+    if (result == -1) {
+        perror("bind error");
+        return 1;
+    }
+    listen(sockfd,5);
 
-    char buffer[1024] = {0};
+    while (1){
+    int client_fd = accept(sockfd, NULL, NULL);
+    if (client_fd == -1){
+        perror("accept error");
+        return 1;
+    }
+    pid_t pid = fork();
 
-    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+        if(pid == 0){
+        // child kısmı, kullanıcı ile bağlantı
 
-    addr.sin_family = AF_INET;
-    addr.sin_addr.s_addr = INADDR_ANY;
-    addr.sin_port = htons(8080);
+    char buffer[1024];
+    int bytes_received = recv(client_fd, buffer, sizeof(buffer), 0);
+    buffer[bytes_received] = '\0';
+    printf("İstemciden gelen: %s\n", buffer);
 
-    bind(server_fd, (struct sockaddr*)&addr, sizeof(addr));
-
-    listen(server_fd, 5);
-
-    printf("Waiting...\n");
-
-    client_fd = accept(server_fd, NULL, NULL);
-
-    recv(client_fd, buffer, sizeof(buffer), 0);
-
-    printf("Message: %s\n", buffer);
-
-    send(client_fd, "Hello Client", 15, 0);
-
+    send(client_fd, "merhaba",7,0);
     close(client_fd);
-    close(server_fd);
+    close(sockfd);
+    exit(0);
+}
+    else {
+        // parent (döngü başa dönmesi için kapatıyor)
+        close (client_fd);
+    }
+    }
 
     return 0;
 }
